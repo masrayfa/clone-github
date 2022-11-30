4ce36e23c9474de84650876ecfd026183cad3686
masrayfa <maselon2030@gmail.com>
2022-11-30 23:16:43 +0700

    add: icon card and icon tab

--- components/CardRepo.tsx
+++ components/CardRepo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CardProps } from '../constants/types'
+import { FiEye } from 'react-icons/fi'
 
 const CardRepo = ({
   description,
@@ -27,9 +28,12 @@ const CardRepo = ({
           {description ? description : 'Description repo not found'}
         </p>
         <div className="flex justify-between">
-          <div className="flex space-x-2">
+          <div className="flex space-x-5 items-center">
             <p className="text-slate-400 text-md">{language}</p>
-            <p className="text-slate-400 text-md">{watchers} watchers</p>
+            <div className="text-slate-400 flex items-center space-x-1">
+              <FiEye />
+              <p>{watchers}</p>
+            </div>
           </div>
           <a href={downloads_url} className="underline text-sm text-green-600">
             download file
